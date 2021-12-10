fb7fa3e18a2beeb0bc8949f8cd89b7e3c9fc407f
Soumya <soumyatarafder624@gmail.com>
2021-12-10 19:16:02 +0530

    Responsive UI

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -3,12 +3,45 @@ import Navbar from "../Navbar/Navbar";
 import Images from "./Images";
 import main from "../../Images/MainImage.png";
 import "./style.css";
+import { Row, Col, Button } from "reactstrap";
+
 export default function Home() {
-	return (
-		<div>
-			<Images />
-			<Navbar />
-			<div className="home">
+  return (
+    <div>
+      <Images />
+      <br />
+      <Row
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "5%",
+        }}
+      >
+        <Col sm="6">
+          <h1 style={{ color: "#727dbd", fontWeight: "bold" }}>
+            Lorem Ipsum Text
+          </h1>
+          <p style={{ borderLeft: "4px solid #727dbd", padding: "2%" }}>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blandit praesentium voluptatum delis atque corrupti quos dolores
+          </p>
+          <Button
+            style={{
+              backgroundColor: "#727dbd",
+              color: "white",
+              border: "none",
+            }}
+          >
+            Get Started
+          </Button>
+          <br />
+        </Col>
+        <Col sm="6">
+          <br />
+          <img src={main} alt="MainImage" style={{ width: "100%" }} />
+        </Col>
+      </Row>
+      {/* <div className="home">
 				<div>
 					<div className="home-text">
 						<h1>Lorem Ipsum Text</h1>
@@ -25,7 +58,7 @@ export default function Home() {
 				<div className="main-image">
 					<img src={main} alt="MainImage" />
 				</div>
-			</div>
-		</div>
-	);
+			</div> */}
+    </div>
+  );
 }

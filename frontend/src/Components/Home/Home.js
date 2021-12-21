b66dd7caa2fcc20879b38f3e2f224112b8db7411
Soumya <soumyatarafder624@gmail.com>
2021-12-22 03:55:26 +0530

    new

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -2,7 +2,28 @@ import React from "react";
 import Images from "./Images";
 import main from "../../Images/MainImage.png";
 import "./style.css";
-import { Row, Col, Button } from "reactstrap";
+import { Row, Col, Button, UncontrolledCarousel } from "reactstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export default function Home() {
   return (
@@ -15,17 +36,23 @@ export default function Home() {
         }}
       >
         <Col sm="6">
-          <h1 style={{ color: "#727dbd", fontWeight:"bolder" }}>
+          <h1 style={{ color: "#727dbd", fontWeight: "bolder" }}>
             Lorem Ipsum Text
           </h1>
-          <p style={{ borderLeft: "4px solid #727dbd", padding: "2%" , color:"black"}}>
+          <p
+            style={{
+              borderLeft: "4px solid #727dbd",
+              padding: "2%",
+              color: "black",
+            }}
+          >
             At vero eos et accusamus et iusto odio dignissimos ducimus qui
             blandit praesentium voluptatum delis atque corrupti quos dolores
           </p>
           <Button
             style={{
-              width:"200px",
-              height:"50px",
+              width: "200px",
+              height: "50px",
               backgroundColor: "#727dbd",
               color: "white",
               border: "none",
@@ -40,24 +67,12 @@ export default function Home() {
           <img src={main} alt="MainImage" style={{ width: "100%" }} />
         </Col>
       </Row>
-      {/* <div className="home">
-				<div>
-					<div className="home-text">
-						<h1>Lorem Ipsum Text</h1>
-						<div className="home-text-cont">
-							<div></div>
-							<p>
-								At vero eos et accusamus et iusto odio dignissimos ducimus qui
-								blandit praesentium voluptatum delis atque corrupti quos dolores
-							</p>
-						</div>
-						<button>Get Started</button>
-					</div>
-				</div>
-				<div className="main-image">
-					<img src={main} alt="MainImage" />
-				</div>
-			</div> */}
+      <Carousel responsive={responsive} style={{backgroundColor:"#7882bd"}}>
+        <div style={{backgroundColor:"#7882bd", alignItems:"center", textAlign:"center", padding:"5%", color:"white"}}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blandit<br/> praesentium voluptatum delis atque corrupti quos dolores et quas</div>
+        <div style={{backgroundColor:"#7882bd", alignItems:"center", textAlign:"center", padding:"5%", color:"white"}}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blandit<br/> praesentium voluptatum delis atque corrupti quos dolores et quas</div>
+        <div style={{backgroundColor:"#7882bd", alignItems:"center", textAlign:"center", padding:"5%", color:"white"}}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blandit<br/> praesentium voluptatum delis atque corrupti quos dolores et quas</div>
+        <div style={{backgroundColor:"#7882bd", alignItems:"center", textAlign:"center", padding:"5%", color:"white"}}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blandit<br/> praesentium voluptatum delis atque corrupti quos dolores et quas</div>
+      </Carousel>
     </div>
   );
 }
